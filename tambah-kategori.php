ef0433c9c703831a654918d254c95ed0379f4164
febyanaa <152267016+febyanaa@users.noreply.github.com>
2026-01-26 14:20:15 +0700

    Add files via upload
    NIH UNTUK LU KASIH PAHAM BOS BOS BOS BOS

---
+++ tambah-kategori.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    HALAMAN DATA KATEGORI
+    <form action="koneksi-kategori.php" method="POST">
+      <div>
+        <label for=""> ID :</label> <br>
+        <input type="text" name= "id"/>
+      </div>
+      <div>
+        <label for=""> Kategori :</label> <br>
+        <input type="text" name= "kategori"/>
+      </div>
+      <button>KIRIM</button>
+    </form>
+</body>
+</html>
