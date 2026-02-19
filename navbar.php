70409d74623e0d725ab2897a8c07e8b3cc0aa7e7
kiyaa <152266385+kiyaaw@users.noreply.github.com>
2026-02-19 18:17:07 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+    .navbar {
+        overflow: hidden;
+        padding: 10px 20px;
+        font-family: Arial. sans-serif;
+    }
+    .navbar a {
+        float: right;
+        color: white;
+        text-decoration: none;
+        padding: 8px 16px;
+        border-radius: 4px;
+        background-color: red;
+        font-weight: bold;
+        transition: background-color 0.3s;
+    }
+    .navbar a:hover {
+        background-color: red;
+    }
+</style>
+</head>
+<body>
+    <div class="navbar">
+        <a href="proses_logout.php"></a>
+    </div>
+</body>
+</html>
